9bd9726bcbda82e242d2c8c0986248b2076b6248
Diego Baltazar <k9999zero@gmail.com>
2024-10-31 21:03:02 -0400

    Create first reactive variable

--- app/src/main/java/com/sistemas/androidgrupog/Views/Login.kt
+++ app/src/main/java/com/sistemas/androidgrupog/Views/Login.kt
@@ -1,10 +1,20 @@
 package com.sistemas.androidgrupog.Views
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextRange
@@ -13,15 +23,42 @@ import androidx.navigation.NavHostController
 
 @Composable
 fun Login(navController:NavHostController) {
-    ElevatedCard(
-        colors = CardDefaults.cardColors(
-            containerColor = Color.Cyan
-        ),
-        elevation = CardDefaults.cardElevation(
-            defaultElevation = 8.dp
-        ),
-        modifier = Modifier.size(width = 240.dp, height = 120.dp)
-    ) {
-
+    var emailText by remember { mutableStateOf("")}
+    Column (
+        modifier = Modifier.padding(
+            start = 35.dp,
+            end = 35.dp,
+            top = 120.dp,
+            bottom = 120.dp
+        ).fillMaxSize()
+    ){
+        ElevatedCard(
+            colors = CardDefaults.cardColors(
+                containerColor = Color.Cyan
+            ),
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 8.dp
+            ),
+            modifier = Modifier.size(width = 270.dp, height = 350.dp)
+        ) {
+            Column {
+                Row {
+                    OutlinedTextField(
+                        value = "",
+                        onValueChange = { },
+                        label = { Text("Email")},
+                        placeholder = { Text("Ingrese su correo")}
+                    )
+                }
+                Row {
+                    OutlinedTextField(
+                        value = "",
+                        onValueChange = { },
+                        label = { Text("Password")},
+                        placeholder = { Text("Ingrese su Constraseña")}
+                    )
+                }
+            }
+        }
     }
 }
